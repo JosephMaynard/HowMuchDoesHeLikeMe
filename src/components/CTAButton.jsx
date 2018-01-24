d0e0e188c3947381a2c9111e0d8f553b0e656f1f
Joseph Maynard <joseph.maynard@gmail.com>
2018-01-24 22:40:55 +1100

    Packages added and updated

---
+++ src/components/CTAButton.jsx
@@ -0,0 +1,14 @@
+ import React from 'react';
+ import './CTAButton.css';
+
+ function CTAButton(props) {
+   return (
+     <button
+       className={props.cancelButton ? 'cancelButton' : 'CTAButton'}
+       disabled={props.disabled}
+       onClick={props.nextstep}
+     >{props.text}</button>
+   );
+ }
+
+ export default CTAButton;
